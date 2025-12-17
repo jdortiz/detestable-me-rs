96665eca8e78707193109e88fa3d1572514f0ff3
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2025-12-17 16:02:09 +0100

    Add static HTTP handler

--- evilmgmt/src/main.rs
+++ evilmgmt/src/main.rs
@@ -1,10 +1,10 @@
-use axum::Router;
+use axum::{Router, routing::get};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     const SERVER_ADDR: &str = "127.0.0.1:8080";
-    let router = Router::new();
+    let router = Router::new().route("/", get(|| async { "Evilness Management" }));
 
     println!("Launching evilmgmt: http://{SERVER_ADDR}");
     let listener = TcpListener::bind(SERVER_ADDR)
